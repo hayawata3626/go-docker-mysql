dad74e733fdab190122e3746c0853f914b887b12
早津渉 <top.men09@gmail.com>
2020-12-29 18:26:26 +0900

    set up the connection between docker and go

---
+++ go.mod
@@ -0,0 +1,9 @@
+module gin_test
+
+go 1.14
+
+require (
+	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,51 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+  "fmt"
+	"os"
+	"log"
+
+  "github.com/joho/godotenv"
+  "github.com/gin-gonic/gin"
+  "github.com/jinzhu/gorm"
+  _ "github.com/go-sql-driver/mysql"
+)
 
 func main() {
+	err := godotenv.Load()
 	r := gin.Default()
+	db := connectDB()
+	defer db.Close()
+
+	if err != nil {
+    log.Fatal("Error loading .env file")
+	}
+
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "hello from go-docker-mysql-example",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+
+func connectDB() * gorm.DB {
+	DBMS     := os.Getenv("DB")
+  USER     := os.Getenv("MYSQL_USER")
+  PASS     := os.Getenv("MYSQL_PASSWORD")
+  PROTOCOL := os.Getenv("PROTOCOL")
+	DBNAME   := os.Getenv("MYSQL_DATABASE")
+
+	CONNECT := USER+":"+PASS+"@"+PROTOCOL+"/"+DBNAME
+	db,err := gorm.Open(DBMS, CONNECT)
+
+	if err != nil {
+    panic(err.Error())
+	}
+
+	fmt.Println("success db connection!!!")
+
+  return db
 }
