3e8982779de659a8ac76c12b36991a1fe0f644e7
早津渉 <top.men09@gmail.com>
2020-12-30 09:39:12 +0900

    add api for users

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module gin_test
 go 1.14
 
 require (
-	github.com/gin-gonic/gin v1.6.3 // indirect
-	github.com/jinzhu/gorm v1.9.16 // indirect
-	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/jinzhu/gorm v1.9.16
+	github.com/joho/godotenv v1.3.0
 )
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -11,21 +12,39 @@ import (
 	"github.com/joho/godotenv"
 )
 
+// User ...
+type User struct {
+	gorm.Model
+	Name  string
+	Email string
+}
+
 func main() {
 	err := godotenv.Load()
 	r := gin.Default()
 	db := connectDB()
-	defer db.Close()
+	db.AutoMigrate(&User{})
+	// user := User{Name: "sample", Email: "sample@sample.jp"}
+	// db.Create(&user)
 
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	defer db.Close()
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "hello from go-docker-mysql-example",
 		})
 	})
+
+	r.GET("/users", func(c *gin.Context) {
+		users := []User{}
+		db.Find(&users)
+		c.JSON(http.StatusOK, users)
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
@@ -36,7 +55,7 @@ func connectDB() *gorm.DB {
 	PROTOCOL := os.Getenv("PROTOCOL")
 	DBNAME := os.Getenv("MYSQL_DATABASE")
 
-	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?parseTime=true"
 	db, err := gorm.Open(DBMS, CONNECT)
 
 	if err != nil {
